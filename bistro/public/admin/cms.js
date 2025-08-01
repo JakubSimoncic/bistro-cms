367b99d3047e081152af1e33aba05349ca207356
JakubSimoncic <jakub.simoncic@seznam.cz>
2025-08-01 18:06:15 +0200

    Update cms.js

--- bistro/public/admin/cms.js
+++ bistro/public/admin/cms.js
@@ -1,1 +1,8 @@
+import CMS from "netlify-cms-app";
+import MarkdownControl from "./custom-widgets/MarkdownControl";
 
+// Registrovat vlastní widget pod jménem "markdown" (přepíšeš vestavěný)
+CMS.registerWidget("markdown", MarkdownControl);
+
+// Spustit CMS
+CMS.init();
