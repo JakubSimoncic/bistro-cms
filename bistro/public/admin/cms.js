aada1ea273573bb5b200474d44883aa53854a75b
JakubSimoncic <jakub.simoncic@seznam.cz>
2025-08-01 18:41:32 +0200

    Update cms.js

--- bistro/public/admin/cms.js
+++ bistro/public/admin/cms.js
@@ -1,29 +1,3 @@
-function MarkdownControl({ value, onChange }) {
-  const textarea = document.createElement('textarea');
-  textarea.style.width = '100%';
-  textarea.style.height = '300px';
-  textarea.style.fontFamily = 'monospace';
-  textarea.style.fontSize = '14px';
-  textarea.value = value || '';
-
-  textarea.addEventListener('input', e => {
-    // Ulož pozici kurzoru
-    const start = textarea.selectionStart;
-    const end = textarea.selectionEnd;
-
-    // Zavolej onChange s novou hodnotou
-    onChange(e.target.value);
-
-    // Nastav kurzor zpět na uložené místo (po vykreslení)
-    requestAnimationFrame(() => {
-      textarea.selectionStart = start;
-      textarea.selectionEnd = end;
-    });
-  });
-
-  return textarea;
-}
-
 function waitForCMS(callback) {
   if (window.CMS) {
     callback();
