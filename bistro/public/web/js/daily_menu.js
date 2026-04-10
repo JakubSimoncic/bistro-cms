22485a1a76993cc6d86fe8e160419c9bd3377581
JakubSimoncic <201454354+JakubSimoncic@users.noreply.github.com>
2026-04-10 15:54:03 +0200

    Create daily_menu.js

---
+++ bistro/public/web/js/daily_menu.js
@@ -0,0 +1,12 @@
+        fetch('/web/pages/menu-en.md')
+                .then(response => response.text())
+                .then(markdown => {
+                    const content = markdown.replace(/^---[\s\S]*?---\s*/, '');
+                    const html = marked.parse(content);
+                    document.getElementById('menu-content').innerHTML = html;
+                })
+                .catch(error => {
+                    document.getElementById('menu-content').innerHTML = 'Nepodařilo se načíst denní menu.';
+                    console.error(error);
+                });
+consol.log("Menu načteno");
