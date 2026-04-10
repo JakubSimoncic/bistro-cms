ab3286504ef322286daec6fb3a067504ef508938
JakubSimoncic <201454354+JakubSimoncic@users.noreply.github.com>
2026-04-10 20:44:42 +0200

    Update denni_menu.js

--- bistro/public/web/js/denni_menu.js
+++ bistro/public/web/js/denni_menu.js
@@ -1,4 +1,4 @@
-        fetch('/web/pages/denni_menu.md')
+        fetch('pages/denni-menu.md')
                 .then(response => response.text())
                 .then(markdown => {
                     const content = markdown.replace(/^---[\s\S]*?---\s*/, '');
