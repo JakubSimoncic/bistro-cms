709dcb629689768d64da98132c8573ea453738a1
JakubSimoncic <201454354+JakubSimoncic@users.noreply.github.com>
2026-04-10 16:57:55 +0200

    Update index.js

--- bistro/public/web/js/index.js
+++ bistro/public/web/js/index.js
@@ -1,1 +1,30 @@
+            // Logika pro Slideshow
+            let slideIndex = 0;
+            showSlides();
 
+            function showSlides() {
+                let slides = document.getElementsByClassName("slide");
+                if (slides.length === 0) return; // Ochrana, pokud by slides neexistovaly
+                for (let i = 0; i < slides.length; i++) {
+                    slides[i].style.display = "none";
+                }
+                slideIndex++;
+                if (slideIndex > slides.length) {slideIndex = 1}
+                slides[slideIndex - 1].style.display = "block";
+                setTimeout(showSlides, 5000);
+            }
+
+            // Logika pro zavření plovoucího videa
+            document.getElementById('cornerVideoClose').onclick = function() {
+                document.getElementById('cornerVideo').style.display = 'none';
+            };
+
+            if (window.netlifyIdentity) {
+                window.netlifyIdentity.on("init", (user) => {
+                    if (!user) {
+                        window.netlifyIdentity.on("login", () => {
+                            document.location.href = "/admin/";
+                        });
+                    }
+                });
+            }
