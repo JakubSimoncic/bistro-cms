afd656187a5c54e976a7fb93c19f93f8ada8c231
JakubSimoncic <201454354+JakubSimoncic@users.noreply.github.com>
2026-05-04 23:38:44 +0200

    Fix image paths in slideshow array

--- bistro/public/web/js/index.js
+++ bistro/public/web/js/index.js
@@ -4,9 +4,9 @@ document.addEventListener('DOMContentLoaded', function() {
     // 1. LOGIKA PRO SLIDESHOW
     const pravyBlok = document.querySelector('.pravy-blok');
     const obrazky = [
-        'web/fotky/slide_3.jpg',
-        'web/fotky/slide_1.jpg',
-        'web/fotky/slide_2.jpg'
+        '/web/fotky/slide_3.jpg',
+        '/web/fotky/slide_1.jpg',
+        '/web/fotky/slide_2.jpg'
     ];
 
     let slideIndex = 0;
